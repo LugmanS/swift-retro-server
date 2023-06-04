6e7b1148ae714c56f47f0022ab623f2d84568d9a
Lugman <lugman.s@grootan.com>
2023-06-04 14:56:20 +0530

    Prisma setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const app = express();
+import express from "express";
 
+const app = express();
 const PORT = process.env.PORT || 8000;
+
+
+
 
 app.listen(PORT, () => console.log('Server started successfully'));
